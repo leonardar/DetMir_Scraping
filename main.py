762fe3520b3ba852012a45c4af3236b9048dba44
leonardar <leonardar88@gmail.com>
2021-11-07 19:21:09 +0300

    исправлены нестабильные куски кода

--- main.py
+++ main.py
@@ -1,101 +1,19 @@
 import sys
-from selenium.webdriver.support.wait import WebDriverWait
+import time
+import platform
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException
 import csv
 
 
-def scrape(category, city):
-    options = webdriver.ChromeOptions()
-    options.add_argument('user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0')
-    options.add_argument('--disable-blink-features=AutomationControlled')
-    # options.headless = True
-    options.add_argument('window-size=1920,1080')
-    service = Service("./chromedriver")
-    driver = webdriver.Chrome(
-        service=service,
-        options=options
-    )
-    try:
-        driver.get("https://www.detmir.ru")
-        driver.implicitly_wait(3)
-
-        print(f'Выбираем город {city}')
-        list_button = driver.find_element(By.XPATH, "//span[contains(text(), 'другой')]")
-        wait = WebDriverWait(driver, 3)
-        wait.until(EC.element_to_be_clickable(list_button)).click()
-        city_button = driver.find_element(By.XPATH, f"//span[contains(text(), '{city}')]")
-        driver.execute_script("arguments[0].click();", city_button)
-
-        print('Выбираем категорию игрушки')
-        shop_category_button = driver.find_element(By.XPATH,
-                                                   '//a[(@href="https://www.detmir.ru/catalog/index/name/igry_i_igrushki/")]')
-        driver.execute_script("arguments[0].click();", shop_category_button)
-
-        print(f'Выбираем категорию {category}')
-        lego_button = driver.find_element(By.XPATH,
-                                          f'//a[(@href="https://www.detmir.ru/catalog/index/name/{category}/")]')
-        driver.execute_script("arguments[0].click();", lego_button)
-
-        driver.implicitly_wait(3)
-
-        print(f'Пролистываем до конца...')
-        while True:
-            try:
-
-                scroll_button = driver.find_element(By.XPATH, f"//div[contains(text(), 'Показать ещё')]")
-                driver.execute_script("arguments[0].click();", scroll_button)
-                driver.implicitly_wait(7)
-            except (NoSuchElementException, StaleElementReferenceException):
-                break
-
-        print(f'Формируем словарь из продуктов')
-        products = {}
-        products_list = driver.find_elements(By.XPATH, '//a[contains(@href, "https://www.detmir.ru/product/index/id")]')
-        for product in products_list:
-            url = product.get_attribute('href')
-            id = url.split('/')[-2]
-            if id not in products:
-                stats = {}
-                driver.implicitly_wait(1)
-                p_tags = product.find_elements(By.TAG_NAME, 'p')
-                if len(p_tags) == 3:
-                    stats['title'] = p_tags[0].text
-                    stats['price'] = p_tags[2].text
-                    stats['promo_price'] = p_tags[1].text
-                elif len(p_tags) == 2:
-                    stats['title'] = p_tags[0].text
-                    stats['price'] = p_tags[1].text
-                    stats['promo_price'] = ''
-                elif len(p_tags) == 1:
-                    stats['title'] = p_tags[0].text
-                    stats['price'] = 'Нет в наличии'
-                    stats['promo_price'] = ''
-                else:
-                    raise Exception(f"Unexpected number of stats {len(p_tags)} for id {id}")
-
-                stats['url'] = url
-                stats['city'] = city
-                products[id] = stats
-                print(f"  ...продукт {id} - {stats['title']} добавлен")
-        print(f'Получено {len(products)} продуктов из категории {category} в регионе {city}')
-        return products
-
-    except TimeoutException as ex:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(f'{exc_tb.tb_lineno}: TimeoutException = {ex}')
-    except NoSuchElementException as ex:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(f'{exc_tb.tb_lineno}: NoSuchElementException = {ex}')
-    except Exception as ex:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        print(f'{exc_tb.tb_lineno}:{exc_type}: Exception = {ex}')
-    finally:
-        driver.close()
-        driver.quit()
+def get_system_prefix():
+    if platform.system() == 'Linux':
+        return 'linux'
+    elif platform.system() == 'Darwin':
+        return 'mac'
+    return 'exe'
 
 
 def fill_csv(category, location, products):
@@ -106,16 +24,113 @@ def fill_csv(category, location, products):
             file_writer.writerow([key, value['title'], value['price'], value['promo_price'], value['url']])
 
 
+def scrape(category, city):
+    options = webdriver.ChromeOptions()
+    options.add_argument('user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0')
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    options.headless = True
+    options.add_argument('window-size=1920,1080')
+    service = Service("./chromedriver")
+    driver = webdriver.Chrome(
+        service=service,
+        options=options
+    )
+
+    products = {}
+    try:
+        driver.get(f"https://www.detmir.ru/catalog/index/name/{category}/")
+        driver.implicitly_wait(5)
+
+        print(f'Выбираем город {city}')
+        region_button = driver.find_element(By.XPATH,
+                                            "//*[@id='app-container']/div[2]/header/div/div[2]/div/div/div[1]/ul/li[2]/div/div/div[1]/div/span")
+        driver.execute_script("arguments[0].click();", region_button)
+        time.sleep(5)
+
+        city_buttons = driver.find_elements(By.XPATH, f"//span[contains(text(), '{city}')]")
+
+        print('Нажимаем на кнопку и проверяем наличие модального окна')
+        try:
+            for elem in city_buttons:
+                driver.execute_script("arguments[0].click();", elem)
+                modal_close_button = driver.find_element(By.XPATH,
+                                                         "//*[@id='tw']/div[1]/div/div/div[2]/div/button")  # кнопка "закрыть модальное окно"
+                if not modal_close_button.is_displayed():  # если кнопка не видна пользователю
+                    break
+        except (StaleElementReferenceException, NoSuchElementException):
+            pass
+
+        time.sleep(5)
+
+        print(f'Пролистываем до конца...')
+        while True:
+            try:
+                scroll_button = driver.find_element(By.XPATH, f"//div[contains(text(), 'Показать ещё')]")
+                driver.execute_script("arguments[0].click();", scroll_button)
+                driver.implicitly_wait(5)
+            except (NoSuchElementException, StaleElementReferenceException):
+                break
+
+        print('Формируем словарь из продуктов')
+        products_list = driver.find_elements(By.XPATH, '//a[contains(@href, "https://www.detmir.ru/product/index/id")]')
+
+        for product in products_list:
+            try:
+                url = product.get_attribute('href')
+                id = url.split('/')[-2]
+                if id not in products:
+                    stats = {}
+                    driver.implicitly_wait(1)
+                    p_tags = product.find_elements(By.TAG_NAME, 'p')
+                    if len(p_tags) == 3:
+                        stats['title'] = p_tags[0].text
+                        stats['price'] = p_tags[2].text
+                        stats['promo_price'] = p_tags[1].text
+                    elif len(p_tags) == 2:
+                        stats['title'] = p_tags[0].text
+                        stats['price'] = p_tags[1].text
+                        stats['promo_price'] = ''
+                    elif len(p_tags) == 1:
+                        stats['title'] = p_tags[0].text
+                        stats['price'] = 'Нет в наличии'
+                        stats['promo_price'] = ''
+                    else:
+                        print(f"Непредвиденное количество элементов {len(p_tags)} for id {id}:")
+                        continue
+
+                    stats['url'] = url
+                    stats['city'] = city
+                    products[id] = stats
+                    print(f"  ...продукт {id} - {stats['title']} добавлен")
+            except (NoSuchElementException, TimeoutException):
+                pass
+
+    except (ConnectionError, ConnectionRefusedError) as ex:
+        print(f'Не удалось подключиться: {ex}')
+    except TimeoutException as ex:
+        print(f'Команда не была выполнена вовремя: {ex}')
+    except NoSuchElementException as ex:
+        print(f'Элемент не был найден: {ex}')
+    except Exception as ex:
+        print(f'Исключение: {ex}')
+    finally:
+        driver.close()
+        driver.quit()
+        print(f'Получено {len(products)} продуктов из категории {category} в регионе {city}')
+        print('Записываем в файл')
+        fill_csv(category, city, products)
+
+
 if __name__ == '__main__':
-    category = 'lego'
-    spb_location = 'Санкт-Петербург и Ленинградская область'
-    msc_location = 'Москва и Московская область'
+    try:
+        category = 'lego'
 
-    products_spb = scrape(category, spb_location)
-    products_msc = scrape(category, msc_location)
+        msc_location = 'Москва и Московская область'
+        spb_location = 'Санкт-Петербург и Ленинградская область'
 
-    print('Сохраняем в файлы')
-    fill_csv(category, spb_location, products_spb)
-    fill_csv(category, msc_location, products_msc)
+        scrape(category, msc_location)
+        scrape(category, spb_location)
 
-    print('Done!')
+        print('Done!')
+    except Exception as ex:
+        print(f'Неперехваченное исключение: {ex}')
